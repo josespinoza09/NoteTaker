b1e3294e8832d2e25c65770e4757ac23084a064d
Jose Espinoza <jose20019004@gmail.com>
2021-02-26 13:01:17 -0500

    added the delete function

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.use(express.json());
 // access files in public folder
 app.use(express.static('public'));
 
-
+// delete an existing note from DB
+app.delete('/api/notes/:id', (req,res) => {
+  console.log('API REQUEST: delete note \#', req.params.id);
+  console.log(req.params);
+  let dbData = JSON.parse(fs.readFileSync('./db/db.json'));
+  dbData = dbData.filter(entry => !(entry.id == req.params.id));
+  fs.writeFileSync('./db/db.json', JSON.stringify(dbData));
+  res.end();
+});
 // add new note to DB
 app.post('/api/notes', (req,res) => {
   console.log('API REQUEST: save a new note', req.body);
