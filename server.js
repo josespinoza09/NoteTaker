2744b6e25238e7ffa578ecf1af843d4cf228edd6
Jose Espinoza <jose20019004@gmail.com>
2021-02-26 13:00:20 -0500

    added the function to add new notes

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // access files in public folder
 app.use(express.static('public'));
+
+
+// add new note to DB
+app.post('/api/notes', (req,res) => {
+  console.log('API REQUEST: save a new note', req.body);
+  let dbData = JSON.parse(fs.readFileSync('./db/db.json'));
+  dbData.push( {id:dbData.length, title:req.body.title, text:req.body.text} );
+  fs.writeFileSync('./db/db.json', JSON.stringify(dbData));
+  res.end();
+});
